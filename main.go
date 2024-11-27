d8cc6c28ea8202d0b414003e3b08f84ce2fe40d7
Geofrey Ernest <geofreyernest@live.com>
2024-11-28 01:28:34 +0300

    store shards containers

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"hash/maphash"
 	"log/slog"
+	"math"
 	"math/bits"
 	"os"
 
@@ -24,6 +25,129 @@ func main() {
 		indexCommand(flag.Arg(1), flag.Arg(2))
 	}
 }
+
+type iter struct {
+	lo    dataKey
+	hi    dataKey
+	it    *pebble.Iterator
+	shard uint64
+}
+
+func (i *iter) reset(co column) bool {
+	i.lo.Set(co, i.shard, 0)
+	i.hi.Set(co, i.shard, math.MaxUint64)
+	return i.it.SeekGE(i.lo[:])
+}
+
+func (i *iter) Valid() bool {
+	return i.it.Valid() &&
+		bytes.Compare(i.it.Key(), i.hi[:]) == -1
+}
+
+func (i *iter) Next() bool {
+	return i.it.Next() && i.Valid()
+}
+
+func (i *iter) BitLen() uint64 {
+	return i.Max() / shardwidth.ShardWidth
+}
+
+func (i *iter) Value() (uint64, *roaring.Container) {
+	key := i.it.Key()
+	return binary.BigEndian.Uint64(key[len(key)-8:]),
+		roaring.DecodeContainer(i.it.Value())
+}
+
+func (i *iter) Max() uint64 {
+	if !i.it.SeekLT(i.hi[:]) {
+		return 0
+	}
+	key := i.it.Key()
+	if bytes.Compare(key, i.lo[:]) == -1 {
+		return 0
+	}
+	ck := binary.BigEndian.Uint64(key[len(key)-8:])
+	value := roaring.LastValueFromEncodedContainer(i.it.Value())
+	return uint64((ck << 16) | uint64(value))
+}
+
+func (i *iter) Row(rowID uint64) *roaring.Bitmap {
+	return i.OffsetRange(
+		shardwidth.ShardWidth*i.shard,
+		shardwidth.ShardWidth*rowID,
+		shardwidth.ShardWidth*(rowID+1),
+	)
+}
+
+func (i *iter) Seek(key uint64) bool {
+	ls := i.lo[:]
+	binary.BigEndian.PutUint64(ls[len(ls)-8:], key)
+	return i.it.SeekGE(ls) && i.Valid()
+}
+
+func (i *iter) ApplyFilter(key uint64, filter roaring.BitmapFilter) (err error) {
+	if !i.Seek(key) {
+		return
+	}
+	var minKey roaring.FilterKey
+
+	for ; i.Valid(); i.it.Next() {
+		dk := i.it.Key()
+		ckey := binary.BigEndian.Uint64(dk[len(dk)-8:])
+		key := roaring.FilterKey(ckey)
+		if key < minKey {
+			continue
+		}
+		// Because ne never delete, we are sure that no empty container is ever
+		// stored. We pass 1 as cardinality to signal that there is bits ina container.
+		//
+		// Filters only use this to check for empty containers.
+		res := filter.ConsiderKey(key, 1)
+		if res.Err != nil {
+			return res.Err
+		}
+		if res.YesKey <= key && res.NoKey <= key {
+			data := roaring.DecodeContainer(i.it.Value())
+			res = filter.ConsiderData(key, data)
+			if res.Err != nil {
+				return res.Err
+			}
+		}
+		minKey = res.NoKey
+		if minKey > key+1 {
+			if !i.Seek(uint64(minKey)) {
+				return nil
+			}
+		}
+	}
+	return nil
+
+}
+
+func (i *iter) OffsetRange(offset, start, endx uint64) *roaring.Bitmap {
+	dieFalse(lowbits(offset) == 0)("low bits set")
+	dieFalse(lowbits(start) == 0)("low bits set")
+	dieFalse(lowbits(endx) == 0)("low bits set")
+
+	other := roaring.NewSliceBitmap()
+	off := highbits(offset)
+	hi0, hi1 := highbits(start), highbits(endx)
+	if !i.Seek(hi0) {
+		return other
+	}
+	for ; i.Valid(); i.it.Next() {
+		key := i.it.Key()
+		ckey := binary.BigEndian.Uint64(key[len(key)-8:])
+		if ckey >= hi1 {
+			break
+		}
+		other.Containers.Put(off+(ckey-hi0), roaring.DecodeContainer(i.it.Value()).Clone())
+	}
+	return other
+}
+
+func highbits(v uint64) uint64 { return v >> 16 }
+func lowbits(v uint64) uint16  { return uint16(v & 0xFFFF) }
 
 func indexCommand(dataPath string, measurementsPath string) {
 	db := die2(pebble.Open(dataPath, nil))("creating idex database path=%q", dataPath)
@@ -66,7 +190,7 @@ func indexCommand(dataPath string, measurementsPath string) {
 		ba.Add(id, station, int64(temp))
 	}
 	ba.Finish()
-	die(db.Compact([]byte{0}, []byte{byte(shards + 1)}, true))("running compaction")
+	die(db.Compact([]byte{0}, []byte{byte(translateKey + 1)}, true))("running compaction")
 }
 
 type prefix byte
@@ -75,7 +199,6 @@ const (
 	data prefix = 1 + iota
 	translateID
 	translateKey
-	shards
 )
 
 type column byte
@@ -83,6 +206,7 @@ type column byte
 const (
 	weather column = 1 + iota
 	measure
+	shards
 )
 
 const (
@@ -156,28 +280,26 @@ func (ba *batch) Add(id uint64, station []byte, measure int64) {
 }
 
 func (ba *batch) Finish() {
-	var b bytes.Buffer
-	ba.shards.WriteTo(&b)
-	die(ba.ba.Set([]byte{byte(shards)}, b.Bytes(), nil))("saving shards index")
+	ba.saveColumn(shards, ba.shards, 0)
 	ba.save()
 	ba.ba.Close()
 }
 
 func (ba *batch) save() {
-	die(ba.saveColumn(weather, ba.columns.weather))("saving weather")
-	die(ba.saveColumn(measure, ba.columns.measure))("saving measure")
+	die(ba.saveColumn(weather, ba.columns.weather, ba.shard))("saving weather")
+	die(ba.saveColumn(measure, ba.columns.measure, ba.shard))("saving measure")
 	die(ba.ba.Commit(nil))("commit batch")
 	ba.columns.weather.Containers.Reset()
 	ba.columns.measure.Containers.Reset()
 	ba.ba = ba.db.NewBatch()
 }
 
-func (ba *batch) saveColumn(col column, ra *roaring.Bitmap) error {
+func (ba *batch) saveColumn(col column, ra *roaring.Bitmap, shard uint64) error {
 	if !ra.Any() {
 		return nil
 	}
 	ra.Optimize()
-	ba.key.Set(col, ba.shard, 0)
+	ba.key.Set(col, shard, 0)
 	itr, _ := ra.Containers.Iterator(0)
 	for itr.Next() {
 		key, value := itr.Value()
@@ -268,6 +390,15 @@ func die(err error) func(msg string, args ...any) {
 		if err != nil {
 			reason := fmt.Sprintf(msg, args...)
 			slog.Error(reason, "err", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func dieFalse(ok bool) func(msg string, args ...any) {
+	return func(msg string, args ...any) {
+		if !ok {
+			slog.Error(msg, args...)
 			os.Exit(1)
 		}
 	}
